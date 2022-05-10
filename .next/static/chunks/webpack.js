e699cf41e6667d9056009b1ac966fb11a0df43c3
mudasiru rasheed <rasheedrtm1@gmail.com>
2022-05-10 12:40:48 +0100

    Bostrap Page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "37b1de530da0c792"; }
+/******/ 		__webpack_require__.h = function() { return "d388dd266ce88e68"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
